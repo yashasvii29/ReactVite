633ee52cd07b6eb8000f28c64790c420e8a3cdd0
yashasvi <yashasviagrawal29@gmail.com>
2024-02-05 12:57:07 +0530

    add memo folder

---
+++ react-vite-continue/src/Components/Yashu.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Yashu() {
+  return <div>Yashu</div>;
+}
+
+export function Merc() {
+  return <h1>Merc</h1>;
+}
+
+export function Bmw() {
+  return <h1>bmw</h1>;
+}
+
+export default Yashu;
